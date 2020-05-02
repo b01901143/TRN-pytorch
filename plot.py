a9eb661172af3501931cb6b6506c55108707f751
b01901143 <b01901143@ntu.edu.tw>
2020-05-01 21:44:31 -0500

    last push

--- plot.py
+++ plot.py
@@ -1,20 +1,20 @@
 import json 
 import matplotlib.pyplot as plt
+import sys
 
-for i in range(1,6):
-    # Opening JSON file 
-    with open("sample"+str(i)+".json", 'r') as openfile: 
-        result = json.load(openfile) 
+# Opening JSON file 
+with open(sys.argv[1], 'r') as openfile: 
+    result = json.load(openfile) 
 
-    time = []
-    prob = []
+time = []
+prob = []
 
-    for pair in result["slipping"]:
-        time.append(pair[0])
-        prob.append(pair[1])
+for pair in result["slipping"]:
+    time.append(pair[0])
+    prob.append(pair[1])
 
-    plt.plot(time, prob, 'o-')
-    plt.title("Prediction Timeline for Sample "+str(i))
-    plt.ylabel('Probability')
-    plt.xlabel('time (sec.)')
-    plt.show()
+plt.plot(time, prob, 'o-')
+plt.title("Prediction Timeline for Sample")
+plt.ylabel('Probability')
+plt.xlabel('time (sec.)')
+plt.savefig(sys.argv[2])
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Replace the variables with your github repo url, repo name, test
+#video name, json named by your UIN
+GIT_REPO_URL="https://github.com/b01901143/TRN-pytorch.git"
+REPO="TRN-pytorch"
+VIDEO="sample1.mp4"
+UIN_JSON="529005673.json"
+UIN_JPG="529005673.jpg"
+git clone $GIT_REPO_URL
+cd $REPO
+#Replace this line with commands for running your test python file.
+echo $VIDEO
+echo "This might take a while..."
+python test_segment.py --output_file timeLabel.json --video_file $VIDEO --weight pretrain/model_42.pth.tar --arch InceptionV3 --dataset moments
+python plot.py timeLabel.json timeLabel.jpg
+#rename the generated timeLabel.json and figure with your UIN.
+cp timeLabel.json $UIN_JSON
+cp timeLabel.jpg $UIN_JPG
+ 
